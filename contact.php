3a3275c2cc2303082e96bb49b8d68037b405d913
amatesa <alejandromatesa@gmail.com>
2024-05-16 19:00:03 +0200

    Add files via upload
    main pages

---
+++ contact.php
@@ -0,0 +1,269 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Salud & Gestion - Contacto</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" type="text/css" href="stylesheet/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+	<link rel="stylesheet" type="text/css" href="stylesheet/responsive.css">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-101940997-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-101940997-1');
+	</script>
+</head>
+	<body class="home">
+
+		<div class="boxed">
+			<header id="header" class="header">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div id="logo" class="logo">
+								<a href="index.html" title="">
+									<img src="images/logo.png" alt="logo Salud & Gestion" />
+								</a>
+							</div>
+							<div class="btn-menu">
+									<span>a</span>
+							</div>
+							<div class="menu-extra">
+								<div class="box-canvas">
+									<span><i class="fa fa-bars" aria-hidden="true"></i></span>
+								</div>
+							</div>
+							<div class="nav-wrap">
+								<nav id="mainnav" class="mainnav">
+									<ul class="menu">
+										<li>
+											<a href="index.html" title="">Inicio</a>
+										</li>
+										<li>
+											<a href="about.html" title="">Nosotros</a>
+											<ul class="sub-menu">
+												<li><a href="about.html" title="">Empresa</a></li>
+												<li><a href="equipo.html" title="">Equipo</a></li>
+												<li><a href="desafios.html" title="">Desafíos actuales</a></li>
+											</ul>
+										</li>
+										<li>
+											<a href="" title="" >Servicios</a>
+											<ul class="sub-menu">
+												<li class="active"><a href="services.html" title="">Consultoria</a></li>
+												<li><a href="cursos.html" title="">Capacitaciones</a></li>
+											</ul>
+										</li>
+										<li>
+											<a href="novedades.html" title="">Novedades </a>
+
+										</li>
+										<li class="active">
+											<a href="contact.php" title="">Contacto</a>
+										</li>
+									</ul>
+								</nav>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<div class="menu-canvas">
+				<span class="delete">x</span>
+				<div class="logo-canvas">
+					<a href="#">
+						<img src="images/logo-canvas.png" alt="">
+					</a>
+				</div>
+				<div class="canvas-content" >
+					<p>Las empresas de salud, tanto pública como privada, están ante un nuevo desafío: la profesionalización de sus servicios, con el fin de lograr que los mismos se brinden en un marco de <strong>calidad-costo</strong> con la satisfacción del <strong>afiliado-paciente</strong>.
+					</p>
+					<ul>
+						<li>
+							<div class="icon">
+								<span class="icon-envelope"></span>
+							</div>
+							<div class="text">
+								<div class="title">
+									Email
+								</div>
+								<p>
+									direccion@medcomp.com.ar
+								</p>
+							</div>
+						</li>
+						<li>
+							<div class="icon">
+								<span class="icon-clock"></span>
+							</div>
+							<div class="text">
+								<div class="title">
+									Horario de atención
+								</div>
+								<p>
+									Lunes - Viernes: 9:00 hs - 18.00 hs
+								</p>
+							</div>
+						</li>
+					</ul>
+					<div class="btn-more">
+						<a href="contact.php">Contactarse</a>
+					</div>
+				</div>
+			</div>
+
+		<section class="flat-row flat-form parallax">
+			<div class="container">
+				<div class="wrap-form">
+					<div class="row">
+						<div class="col-sm-7">
+							<div class="text-form">
+								<div class="text-title">
+									<h2>CONTACTANOS</h2>
+								</div>
+								<div class="text-content">
+									<p>Usted podrá contar con nuestro asesoramiento en forma Online. Sólo debe llenar el formulario que se acompaña y enviar. En breve tendrá la respuesta al mail que indique.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-5">
+							<div class="contact-form">
+								<form name="frmContacto" id="contact-form" method="post" action="sendmail.php" class="form-contact-form">
+								    <div class="form-group">
+								      	<input class="" type="text" name="name" id="needle" placeholder="Nombre" required autofocus>
+								    </div>
+								    <div class="form-group">
+								      	<input class="" type="email" name="email" id="needle" placeholder="E-mail" required>
+								    </div>
+								    <div class="form-group">
+												<input class="" type="text" name="titulo" id="needle" placeholder="Motivo" required>
+								    </div>
+								    <div class="form-group">
+								      	<textarea name="comment" id="" placeholder="Realice su consulta"></textarea>
+								    </div>
+								    <button type="submit" value="Enviar">Enviar</button>
+								 </form>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="overlay"></div>
+		</section>
+
+		<!-- <section class="flat-row flat-contact">
+			<div class="container">
+				<div class="row contact-container">
+					<div class="contact-item center border">
+						<div class="contact-title">
+							OFICINAS CAP. FEDERAL
+						</div>
+						<ul class="contact-list">
+							<li class="address">Suipacha 207, Capital Federal, Buenos Aires</li>
+							<li class="open">Lunes a Viernes: 09:00 a 18:00</li>
+							<li class="email">info@saludgestion.com.ar</li>
+							<li class="phone">011 5217-3949/50</li>
+						</ul>
+					</div>
+					<iframe id="mapa-google" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6567.890982224395!2d-58.38137401211474!3d-34.605539843763054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5af3653b65fa5b36!2sSalud+y+Gesti%C3%B3n+S.A.!5e0!3m2!1ses-419!2sar!4v1518186978472" width="500" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+			</div>
+
+		</section> -->
+
+
+		<footer class="style2">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 col-sm-6">
+						<div class="widget-footer widget-about">
+							<div class="logo-ft">
+								<a href="index.html" title="logo Salud & Gestion">
+									<img src="images/logo-ft.png" alt="">
+								</a>
+							</div>
+							<div class="text-about">
+								<p>Desarrollo y profesionalización de las áreas  de auditoria-médica, administrativas y financieras. </p>
+								<p>Manejo de herramientas de gestión, desarrollo y mejora de los sistemas de control y facturación.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6">
+						<div class="widget-footer widget-quicklink">
+							<h3 class="widget-title">
+								MENÚ
+							</h3>
+							<ul class="one-half">
+								<li><a href="index.html" title="">Inicio</a></li>
+								<li><a href="about.html" title="">Nosotros</a></li>
+								<li><a href="services.html" title="">Servicios</a></li>
+							</ul>
+							<ul class="one-half second">
+								<li><a href="novedades.html" title="">Novedades</a></li>
+								<li><a href="contact.php" title="">Contacto</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-md-3 col-sm-6">
+						<div class="widget-footer widget-contact">
+							<h3 class="widget-title">CONTACTANOS</h3>
+							<ul class="footer-info">
+								<!-- <li class="address">Suipacha 207, Capital Federal, Buenos Aires</li> -->
+								<li class="email">direccion@medcomp.com.ar</li>
+								<!-- <li class="phone">
+									<span>011 5217-3950</span>
+									<span>011 5217-3949</span>
+								</li> -->
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+		<section class="footer-bottom style2">
+			<div class="container">
+				<div class=design>
+						<p>A&M - Design</p>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="social-footer">
+							<li class="linkedin"><a href="https://www.linkedin.com/in/saludygestion-sa/" title=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+							<li class="facebook"><a href="https://www.facebook.com/syg.auditoria/" title=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li class="google"><a href="https://plus.google.com/u/0/102939445648595848669" title=""><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+						<!-- <div class="copyright">
+							Copyright © 2017 - <a href="#" title="">SyG</a>. Derechos reservados
+						</div> -->
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="button-go-top">
+			<a href="#" title="" class="go-top">
+				<i class="fa fa-chevron-up"></i>
+			</a>
+		</div>
+
+	</div>
+
+	<!-- Javascript -->
+  	<script type="text/javascript" src="javascript/jquery.min.js"></script>
+  	<script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+		<script type="text/javascript" src="javascript/waypoints.min.js"></script>
+		<script type="text/javascript" src="javascript/main.js"></script>
+
+	</body>
+</html>
